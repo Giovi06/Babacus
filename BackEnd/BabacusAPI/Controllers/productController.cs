4217b8c2fec53bd0ea49caa9dbe9c58445dc561e
GM21GM <111045891+GM21GM@users.noreply.github.com>
2024-03-13 09:16:11 +0100

    BabacusDb Model

--- BackEnd/BabacusAPI/Controllers/productController.cs
+++ BackEnd/BabacusAPI/Controllers/productController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using BabacusAPI.Models;
 
 namespace BabacusAPI.Controllers
 {
@@ -6,6 +9,78 @@ namespace BabacusAPI.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        // Controller actions will be added here
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _products.Add(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+        [HttpPost]
+        public IActionResult NewBoughtProduct(Product product)
+        {
+            _products.Add(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+        [HttpPost]
+        public IActionResult NewSoldProduct(Product product)
+        {
+            _products.Add(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProductInfo(int id, Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            product.Description = updatedProduct.Description;
+            product.supplierId = updatedProduct.supplierId;
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProductStock(int id, Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Stock = updatedProduct.Stock;
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _products.Remove(product);
+            return NoContent();
+        }
     }
 }
--- BackEnd/BabacusAPI/Model/ProductDTO.cs
+++ BackEnd/BabacusAPI/Model/ProductDTO.cs
@@ -1,6 +1,6 @@
 public class ProductDTO
 {
-    public int id { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
