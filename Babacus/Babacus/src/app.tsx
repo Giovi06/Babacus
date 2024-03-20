263504a5c2b412987868495244edc7f4cfb0617c
Mirhan <mirhan.oez@gmail.com>
2024-03-20 08:47:47 +0100

    34
    34

--- Babacus/Babacus/src/app.tsx
+++ Babacus/Babacus/src/app.tsx
@@ -2,47 +2,31 @@ import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
 
-
-
-// Interface für die Todo-Objekte
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
-
-// Komponente für die Todo-Liste
-const TodoList: React.FC<{ todos: Todo[] }> = ({ todos }) => (
-  <ul>
-    {todos.map(todo => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
-);
-
-// Hauptkomponente für die Todo-App
-const TodoApp: React.FC = () => {
-  // Zustand für die Todo-Liste
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  // Funktion zum Hinzufügen einer neuen Todo
-  const addTodo = () => {
-    const newTodo: Todo = {
-      id: todos.length + 1,
-      text: `Todo ${todos.length + 1}`,
-      completed: false,
-    };
-    setTodos(prevTodos => [...prevTodos, newTodo]);
-  };
+export function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <TodoList todos={todos} />
-      <button onClick={addTodo}>Add Todo</button>
-    </div>
-  );
-};
-
-export default TodoApp;
-
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  )
+}
