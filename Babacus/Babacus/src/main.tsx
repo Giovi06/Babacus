1c68365bc78dec76d9e66d7bbb179bcb8a3f089b
LukasHeiniger <111046378+LukasHeiniger@users.noreply.github.com>
2024-03-20 08:45:04 +0100

    Babacus new Version

--- Babacus/Babacus/src/main.tsx
+++ Babacus/Babacus/src/main.tsx
@@ -1,5 +1,5 @@
-import { render } from 'preact'
-import { App } from './app'
-import 'bootstrap/dist/css/bootstrap.css'
+import { render } from "preact";
+import App from "./app";
+import "bootstrap/dist/css/bootstrap.css";
 
-render(<App />, document.getElementById('app') as HTMLElement)
+render(<App />, document.getElementById("app") as HTMLElement);
