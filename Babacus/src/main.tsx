54e2b86343affde6c43a741b089b53b1be785c02
LukasHeiniger <111046378+LukasHeiniger@users.noreply.github.com>
2024-03-27 09:09:50 +0100

    Sigma W

--- Babacus/src/main.tsx
+++ Babacus/src/main.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Einkauf";
+import Einkauf from "./Einkauf";
+import Verkauf from "./Verkauf";
+
+const Main: React.FC = () => {
+  const [isEinkaufLoaded, setIsEinkaufLoaded] = useState(false);
+
+  const handleEinkaufClick = () => {
+    setIsEinkaufLoaded(true);
+  };
+
+  const handleVerkaufClick = () => {
+    setIsEinkaufLoaded(false);
+  };
+
+  return (
+    <div>
+      <button onClick={handleEinkaufClick}>Einkauf laden</button>
+      <button onClick={handleVerkaufClick}>Verkauf laden</button>
+
+      {isEinkaufLoaded ? <Einkauf /> : <Verkauf />}
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
