d7c48a33d973094fc07a84de2d314b1982a79485
Mirhan <mirhan.oez@gmail.com>
2024-03-27 08:40:39 +0100

    Css für einkauf

--- Babacus/src/Einkauf.tsx
+++ Babacus/src/Einkauf.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Einkauf.css";
 
 interface Product {
   id: string;
--- Babacus/src/Verkauf.tsx
+++ Babacus/src/Verkauf.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./Verkauf.css";
 
 interface Item {
   id: number;
